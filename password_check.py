f30014280689341ca738d121dd11ccbfd6b41a5e
hoangtuanvp2011 <t00aienl@gmail.com>
2020-11-29 15:41:16 +0700

    begin writing the program

---
+++ password_check.py
@@ -0,0 +1,16 @@
+def CheckPass(Min):
+    Password = input("Your password is :")
+    while len(Password) < Min:
+        print("please enter password again")
+        Password = input("The password must be {0} or more than {0} characters".format(Min))
+    return Password
+
+
+def main():
+    Min = 8
+    AvailablePass = CheckPass(Min)
+    print(len(AvailablePass) * '*')
+
+
+if __name__ == '__main__':
+    main()
